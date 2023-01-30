a4024db2798bd61e03a3e912f6235886aaa79bf1
Leon Lin <leonl0617@gmail.com>
2023-01-30 22:39:10 +0800

    version 2

--- guess-num.py
+++ guess-num.py
@@ -1,11 +1,16 @@
-# 產生一個隨機整數1~100 (不要印出來)
+# 讓使用者決定隨機變數範圍 (不要印出來)
 # 讓使用者重複輸入數字去猜
 # 猜對了的話印出 “終於猜對了!"
 # 猜錯的話 要告訴他比答案大/小
 
 import random
 
-r = random.randint(1 , 100)
+start = input("請輸入要猜的起始數字：")
+start = int(start)
+end = input("請輸入要猜的結束數字：")
+end = int(end)
+
+r = random.randint(start , end)
 
 while True:
    num = input("請猜數字： ")
